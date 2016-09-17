ea2af433dba898db61e0ec1ffd298a9a3425ac9e
Yuriy Grynkov <yugrinkov@gmail.com>
2016-09-17 21:45:33 +0300

    Modificated service implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { HttpModule }    from '@angular/http';
 
 import { routing } from './app.routing';
 
@@ -14,6 +15,7 @@ import { HeroService} from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
     routing
   ],
   declarations: [
--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,12 +1,25 @@
 import { Injectable } from '@angular/core';
+import { Headers, Http } from '@angular/http';
+
+import 'rxjs/add/operator/toPromise';
 
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
 
 @Injectable()
 export class HeroService {
+   private heroesUrl = 'https://jsonplaceholder.typicode.com/users'
+   constructor(private http: Http){ }
+
     getHeroes(): Promise<Hero[]> {
-      return Promise.resolve(HEROES);
+      return this.http.get(this.heroesUrl)
+                 .toPromise()
+                 .then(response => response.json() as Hero[])
+                 .catch(this.handleError);
+    }
+
+    private handleError(error:any): Promise<any>{
+      console.error('An error occured', error);
+      return Promise.reject(error.message || error);
     }
 
     getHero(id: number): Promise<Hero>{
